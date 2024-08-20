f1289b156fd08da6bffffc721f15e8d49763d1bd
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-08-20 11:19:27 -0300

    fixes

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Adicione aqui
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -30,6 +31,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 ROOT_URLCONF = "twitterclone.urls"
 
@@ -62,7 +64,7 @@ if ENVIRONMENT == 'production':
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'sumioshi$default',  # Nome do seu banco de dados
             'USER': 'sumioshi',  # Seu nome de usuário no PythonAnywhere
-            'PASSWORD': 'twitterclone',  # A senha que você configurou para o MySQL no PythonAnywhere
+            'PASSWORD': 'sumioshi',  # A senha que você configurou para o MySQL no PythonAnywhere
             'HOST': 'sumioshi.mysql.pythonanywhere-services.com',
             'PORT': '3306',
         }
@@ -107,8 +109,10 @@ USE_TZ = True
 # Arquivos estáticos
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'core/static']  # Ajustei para o diretório onde seus arquivos CSS estão localizados
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Diretório onde os arquivos estáticos serão coletados para produção
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'core/static']
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Configurações adicionais
 
